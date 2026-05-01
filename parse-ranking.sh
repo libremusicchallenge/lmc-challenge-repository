198a4af4f64f638866bb919714094f553186e383
ASubLin <asublin@gmail.com>
2026-05-01 13:29:42 +0300

    Almost complete parse_ranking.sh

--- parse-ranking.sh
+++ parse-ranking.sh
@@ -18,12 +18,32 @@ else
     exit 1
 fi
 
+present_challenge="$(get_present_challenge)"
+mapfile -t artists < <(get_submission_artists "$present_challenge")
+mapfile -t tracks < <(get_submission_tracks "$present_challenge")
 mapfile -t ranking < "$ranking_fp"
 
 for i in "${!ranking[@]}"; do
-    if [[ ${ranking[$i]} == *$((i + 1))* ]]; then
-        echo "i=$i, ranking[$i]=${ranking[$i]}"
+    r="$((i+1))"
+    # Check that each line starts with its proper ranking
+    if [[ "${ranking[$i]}" == $r* ]]; then
+        ranking_added=no
+        # Check that each line corresponds to a submission
+        for j in ${!artists[@]}; do
+            a="${artists[$j]}"
+            t="${tracks[$j]}"
+            if [[ "${ranking[$i]}" == *"$a"* || "${ranking[$i]}" == *"$t"* ]]; then
+                log_info "Found ranking: ${r}th: $a - $t"
+                # NEXT: fix structure, see LMC38 for correct structure
+                jq --arg r "$r" --arg a "$a" --arg t "$t" '.votes += [{$r: {artist: $a, track: $t}}]' "$present_challenge" > "${present_challenge}.$$" && mv "${present_challenge}.$$" "$present_challenge"
+                ranking_added=yes
+                break
+            fi
+        done
+        if [[ $ranking_added == no ]]; then
+            log_error "Cannot find ranking $r at line $i, within content \"${ranking[$i]}\".  Abort parsing, please fix the file."
+        fi
     else
-        log_error "Cannot find ranking $((i + 1)) at line $i, within content \"${ranking[$i]}\".  Abort parsing, please fix the file."
+        log_error "Cannot find ranking $r at line $i, within content \"${ranking[$i]}\".  Abort parsing, please fix the file."
     fi
 done
